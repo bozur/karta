1b31c92ecf4ce2089348f48a359919e60250e714
bozur <bozur.vujicic@gmail.com>
2025-11-28 11:50:01 +0100

    prijedlozi renamed to opste

---
+++ sections/opste.js
@@ -0,0 +1,13 @@
+// Opste (General) section functionality
+
+// This code is executed when opste section is loaded
+$(document).ready(function () {
+    // Add general statistics functionality here
+    console.log('Opste section loaded');
+
+    // TODO: Display user count
+    // TODO: Show last 10 contributions with amounts
+    // TODO: Display statistics (items, themes, comments, events, records)
+    // TODO: Show items added since last visit
+    // TODO: Show pending approvals
+});
